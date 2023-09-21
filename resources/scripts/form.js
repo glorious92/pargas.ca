2e9daa12f3a89a0f9431674ddd72b1efd4dcca86
Sina BHR <m.bahiraie92@gmail.com>
2023-09-21 15:05:44 +0400

    Update form info

--- resources/scripts/form.js
+++ resources/scripts/form.js
@@ -4,14 +4,18 @@ const isSingleRadio = document.getElementById("is-single");
 const isDivorcedRadio = document.getElementById("is-divorced");
 const isPartnerMarriedRadio = document.getElementById(`is-married-partner`);
 const partnerPartnerClient = document.querySelector(`#partner-client .client-marriage`);
-
-
-
 isMarriedRadio.addEventListener("change", togglePartnerClient);
 isSingleRadio.addEventListener("change", togglePartnerClient);
 isDivorcedRadio.addEventListener("change", togglePartnerClient);
 
-togglePartnerClient();
+
+// const contactForm = document.getElementById('contact-form');
+const notification = document.getElementById('notification');
+
+
+
+
+//  checking marriage ////////////////////////////////////////////////////////////////////////
 
 function togglePartnerClient() {
     if (isMarriedRadio.checked) {
@@ -36,3 +40,72 @@ function togglePartnerClient() {
     }
 
 }
+togglePartnerClient();
+
+
+
+
+
+// submit form //////////////////////////////////////////////////////////////////////////////
+
+// notification
+function showNotification(message, duration) {
+    notification.textContent = message;
+    notification.style.display = 'block';
+
+    setTimeout(function () {
+        notification.style.display = 'none';
+    }, duration);
+}
+
+
+// form submission
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    // You can perform any additional actions here before submitting to Google Forms
+
+    // Submit the form to Google Forms programmatically using JavaScript
+    const form = e.target;
+    const formData = new FormData(form);
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('POST', form.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // // Display the success message
+    // const successMessage = document.getElementById('success-message');
+    // successMessage.style.display = 'block';
+    showNotification('اطلاعات شما با موفقیت ارسال شد . . .', 2000);
+
+    // // Clear the form fields
+    form.reset();
+
+    // // Hide the success message after 2 seconds
+    // setTimeout(function () {
+    //     successMessage.style.display = 'none';
+    // }, 5000);
+
+
+
+    // alert(`message sent!`);
+
+    // xhr.onreadystatechange = function () {
+    //     if (xhr.readyState === XMLHttpRequest.DONE) {
+    //         if (xhr.status === 200) {
+    //             // Form submission was successful
+
+    //         } else {
+    //             // Form submission failed
+    //             // You can add your own error handling code here
+    //             console.error('Form submission failed');
+    //         }
+    //     }
+    // };
+
+    // Convert the form data to URL-encoded format
+    const encodedData = new URLSearchParams(formData).toString();
+
+    // Send the POST request
+    xhr.send(encodedData);
+});
